9bfdd0106c9e66ad020a8182baa2af4991cb83c5
Jayanth Komarraju <jay.komarraju@gmail.com>
2024-02-11 09:08:53 -0500

    Build 0.1

--- frame/app/api/frame/route.ts
+++ frame/app/api/frame/route.ts
@@ -7,27 +7,27 @@
 
 //     const nextId = idAsNumber + 1
 
-//     if (idAsNumber === 12) {
-//         return new NextResponse(`<!DOCTYPE html><html><head>
-//         <title>This is frame 7</title>
-//         <meta property="fc:frame" content="vNext" />
-//         <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/" />
-//         <meta property="fc:frame:button:1" content="Visit Politio" />
-//         <meta property="fc:frame:button:1:action" content="post_redirect" />
-//         <meta property="fc:frame:button:2" content="Learn more" />
-//         <meta property="fc:frame:button:2:action" content="post_redirect" />
-//         <meta property="fc:frame:postUrl" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
-//         </head></html>`)
+    // if (idAsNumber === 12) {
+    //     return new NextResponse(`<!DOCTYPE html><html><head>
+    //     <title>This is frame 7</title>
+    //     <meta property="fc:frame" content="vNext" />
+    //     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/" />
+    //     <meta property="fc:frame:button:1" content="Visit Politio" />
+    //     <meta property="fc:frame:button:1:action" content="post_redirect" />
+    //     <meta property="fc:frame:button:2" content="Learn more" />
+    //     <meta property="fc:frame:button:2:action" content="post_redirect" />
+    //     <meta property="fc:frame:postUrl" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+    //     </head></html>`)
 
-//     } else {
-//         return new NextResponse(`<!DOCTYPE html><html><head>
-//         <title>This is frame ${id}</title>
-//         <meta property="fc:frame" content="vNext" />
-//         <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}" />
-//         <meta property="fc:frame:button:1" content="Next Page" />
-//         <meta property="fc:frame:postUrl" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
-//         </head></html>`)
-//     }
+    // } else {
+    //     return new NextResponse(`<!DOCTYPE html><html><head>
+    //     <title>This is frame ${id}</title>
+    //     <meta property="fc:frame" content="vNext" />
+    //     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}" />
+    //     <meta property="fc:frame:button:1" content="Next Page" />
+    //     <meta property="fc:frame:postUrl" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+    //     </head></html>`)
+    // }
 
 
 // }
@@ -53,27 +53,38 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const searchParams = new URL(req.url).searchParams;
     const id = searchParams.get('id') ?? '';
   const nextId = id + 1;
+    const idAsNumber = parseInt(id);
 //   const imageUrl = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qmaa9vQwMY6RrYgYgnsj7s5dYVfZFqds87moMv83o7GxiU/${imageNames[id % imageNames.length]}`;
 //   The left-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.ts(2362)
     const imageUrl = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qmaa9vQwMY6RrYgYgnsj7s5dYVfZFqds87moMv83o7GxiU/${imageNames[parseInt(id) % imageNames.length]}`;
 
-  // Simplified HTML response for demonstration
-  const htmlResponse = `<!DOCTYPE html><html><head>
-    <title>This is frame ${id}</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${imageUrl}" />
-    <meta property="fc:frame:button:1" content="Next Page" />
-    <meta property="fc:frame:postUrl" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
-    </head></html>`;
+    if (idAsNumber === 12) {
+        return new NextResponse(`<!DOCTYPE html><html><head>
+        <title>This is frame 7</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/" />
+        <meta property="fc:frame:button:1" content="Visit Politio" />
+        <meta property="fc:frame:button:1:action" content="post_redirect" />
+        <meta property="fc:frame:button:2" content="Learn more" />
+        <meta property="fc:frame:button:2:action" content="post_redirect" />
+        <meta property="fc:frame:postUrl" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+        </head></html>`)
 
-  return new NextResponse(htmlResponse, {
-    headers: { 'Content-Type': 'text/html' },
-  });
+    } else {
+        return new NextResponse(`<!DOCTYPE html><html><head>
+        <title>This is frame ${id}</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}" />
+        <meta property="fc:frame:button:1" content="Next Page" />
+        <meta property="fc:frame:postUrl" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+        </head></html>`)
+    }
+
 }
 
-// Annotated POST function
+
 export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req);
+    return getResponse(req);
 }
 
 // Export the dynamic property
